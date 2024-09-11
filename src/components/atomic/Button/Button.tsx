ac533e1e17673282a8f9df84d406353dea0202a4
antoniowav <hello@antoniopiattelli.com>
2024-09-11 10:10:25 +0200

    created some variables and fixed some design issues

--- src/components/atomic/Button/Button.tsx
+++ src/components/atomic/Button/Button.tsx
@@ -10,9 +10,18 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ title, type, onClick }) => {
-    if (type === "primary") return <button className="button__primary" onClick={onClick}>{title}<Arrow className="button__arrow" /></button>;
-    //NOTE: not yet a style for secondary
-    if (type === "secondary") return <button className="button__secondary" onClick={onClick}>{title}</button>;
+    if (type === "primary") {
+        return (
+            <button className="button__primary" onClick={onClick}>
+                {title}
+                <Arrow className="button__arrow" />
+            </button>
+        );
+    }
+    // NOTE: not yet a style for secondary
+    if (type === "secondary") {
+        return <button className="button__secondary" onClick={onClick}>{title}</button>;
+    }
 
     return <button className="button__primary" onClick={onClick}>{title}</button>;
 };
